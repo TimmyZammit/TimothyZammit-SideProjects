36280f2dbe4e0c2ae0933f976ef90cf699896543
TimmyZammit <timmyzammit@icloud.com>
2024-06-11 14:02:50 +0200

    links finally fixed
    created base URL

--- scripts/headerController.js
+++ scripts/headerController.js
@@ -1,6 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const baseHref = location.hostname === '127.0.0.1' ? 'http://127.0.0.1:5500/' : 'https://timmyzammit.github.io/TimothyZammit-SideProjects/';
+    document.querySelector('base').href = baseHref;
+
+    loadHeaderAndInitialize();
+});
+
 // Load header and then initialize header functionalities
 function loadHeaderAndInitialize() {
-    fetch('widgets/header.html')
+    fetch('templates/widgets/header.html')
         .then(response => response.text())
         .then(html => {
             document.getElementById('header-placeholder').innerHTML = html;
